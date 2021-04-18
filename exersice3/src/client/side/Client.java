380349ee704506be36a523355d4ae17a0f48b42e
syafi <syafi@ASUS-HEROIII>
2021-04-18 22:18:02 +0800

    Update Java

--- exersice3/src/client/side/Client.java
+++ exersice3/src/client/side/Client.java
@@ -8,11 +8,30 @@ public class Client {
 	public static void main(String[] args) throws IOException {
 		
 		Socket s = new Socket("localhost", 4999);
+		String text = "Universiti Teknologi Malaysia Melaka";
 		
+		// Text view in client
+		System.out.println(text);
+		
+		// Send to server
 		PrintWriter pr = new PrintWriter(s.getOutputStream());
-		pr.println("Whatzuppp");
+		pr.println(wordcount(text));
 		pr.flush();
 		
-		s.close();
+		s.close();	
 	}
+	
+	static int wordcount(String string)  
+    {  
+      int count=0;  
+  
+        char ch[]= new char[string.length()];     
+        for(int i=0;i<string.length();i++)  
+        {  
+            ch[i]= string.charAt(i);  
+            if( ((i>0)&&(ch[i]!=' ')&&(ch[i-1]==' ')) || ((ch[0]!=' ')&&(i==0)) )  
+                count++;  
+        }  
+        return count;  
+    }
 }
--- exersice3/src/server/side/Server.java
+++ exersice3/src/server/side/Server.java
@@ -8,18 +8,19 @@ public class Server {
 	public static void main(String[] args) throws IOException {
 		
 		ServerSocket ss = new ServerSocket(4999);
+		System.out.println("Server Running");
 		
-			Socket s = ss.accept();
+		Socket s = ss.accept();
 			
-			 System.out.println(" The Client" + " " + s.getInetAddress()
-             + ":" + s.getPort() + " IS CONNECTED ");
+		System.out.println("The Client" + " " + s.getInetAddress()
+           + ":" + s.getPort() + " IS CONNECTED ");
 			 
-			 InputStreamReader in = new InputStreamReader(s.getInputStream());
-			 BufferedReader bf = new BufferedReader(in);
+		InputStreamReader in = new InputStreamReader(s.getInputStream());
+		BufferedReader bf = new BufferedReader(in);
 			 
-			 String str = bf.readLine();
-			 System.out.println("From Client : " + str);
+		String str = bf.readLine();
+		System.out.println("From Client : Total words detect in client are: " + str + " words.");
 			 
-			 ss.close();
+		ss.close();
 	}
 }
